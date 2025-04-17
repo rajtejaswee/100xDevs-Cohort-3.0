a90c72f75dd683849c6e5a1614b3e72fedbaa507
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-04-17 18:57:58 +0530

    http servers

--- HTTP Servers/index.js
+++ HTTP Servers/index.js
@@ -1,45 +1,93 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+// const app = express();
 
-var users = [
-  {
-    name: "John",
-    kidneys: [
-      {
-        healthy: false,
-      },
-    ],
-  },
-];
+// var users = [
+//   {
+//     name: "John",
+//     kidneys: [
+//       {
+//         healthy: false,
+//       },
+//     ],
+//   },
+// ];
 
-app.get('/', function (req, res) {
-    const johnKidneys = users[0].kidneys;
-    const numberOfKidneys = johnKidneys.length;
-    let numberOfHealthyKidneys = 0;
-    for (let i = 0; i < johnKidneys.length; i++) {
-        if (johnKidneys[i].healthy) {
-            numberOfHealthyKidneys++;
-        }
-    }
-    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+// app.get('/', function (req, res) {
+//     const johnKidneys = users[0].kidneys;
+//     const numberOfKidneys = johnKidneys.length;
+//     let numberOfHealthyKidneys = 0;
+//     for (let i = 0; i < johnKidneys.length; i++) {
+//         if (johnKidneys[i].healthy) {
+//             numberOfHealthyKidneys++;
+//         }
+//     }
+//     const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+//     res.json({
+//         numberOfKidneys,
+//         numberOfHealthyKidneys,
+//         numberOfUnhealthyKidneys,
+//     })
+// })
+
+// app.post('/', function (req, res) {
+    
+// })
+
+// app.put('/', function (req, res) {
+    
+// })
+
+// app.delete('/', function (req, res) {
+    
+// })
+
+// app.listen(3000)
+
+// Assignment
+
+const express = require('express') 
+
+const app = express()
+
+app.get('/sum', function (req, res) {
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
     res.json({
-        numberOfKidneys,
-        numberOfHealthyKidneys,
-        numberOfUnhealthyKidneys,
+        ans: a + b
     })
 })
 
-app.post('/', function (req, res) {
-    
+
+app.get('/difference', function (req, res) {
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
+    res.json({
+        ans: a - b
+    })
 })
 
-app.put('/', function (req, res) {
-    
+
+
+app.get('/multiply', function (req, res) {
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
+    res.json({
+        ans: a * b
+    })
 })
 
-app.delete('/', function (req, res) {
-    
+
+
+app.get('/divide', function (req, res) {
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
+    res.json({
+        ans: a / b
+    })
 })
+
+
+
 
 app.listen(3000)
